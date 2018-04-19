737bca8bdb11399ae2eedc5fd4ac0c2f291b7e35
Nicholas Wydra <nick.wydra@antares.com>
2018-04-19 15:44:46 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { WorkComponent } from './work/work.component';
+import { WorkListComponent } from './work/work-list/work-list.component';
+import { WorkDetailComponent } from './work/work-detail/work-detail.component';
+import { WorkItemComponent } from './work/work-list/work-item/work-item.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    WorkComponent,
+    WorkListComponent,
+    WorkDetailComponent,
+    WorkItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
